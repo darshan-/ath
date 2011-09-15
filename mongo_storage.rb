9b67d0f8cd1c0c707d1bbc52c8a23bb077eb1f2a
Darshan-Josiah Barber <darshan@darshancomputing.com>
2011-09-15 23:45:01 +0000

    Start thinking about Mongo.
    git-svn-id: file:///home/darshan/svn-tmp-repo/translationhelper@480 615dbf4c-cc44-46bf-a361-beab69ac16e6

---
+++ mongo_storage.rb
@@ -0,0 +1,17 @@
+require 'mongo'
+
+class MongoStorage
+  def initialize()
+    @db = Mongo::Connection.new.db("test_ath_bi")
+  end
+
+  def get_langs()
+    @db.collection_names.delete_if {|i| i =~ /\./} - ['en'] # Mongo system collections all have a dot
+  end
+
+  def get_string(lang, name)
+  end
+
+  def put_string(lang, name, string)
+  end
+end

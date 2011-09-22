748855f369456d42f001a25d9a2cce9810b54c30
Darshan-Josiah Barber <darshan@darshancomputing.com>
2011-09-21 21:45:01 -0700

    Get MongoStorage working.

--- mongo_storage.rb
+++ mongo_storage.rb
@@ -9,13 +9,48 @@ class MongoStorage
   end
 
   def get_langs()
-    @db.collection_names.delete_if {|i| i =~ /\./} - ['en'] # Mongo system collections all have a dot
+    @db.collection_names.to_a.delete_if {|i| i =~ /\./} - ['en'] # Mongo system collections all have a dot
   end
 
   def get_strings(lang)
+    #strings = {}
+    #str_ars = {}
+    #str_pls = {}
+
+    c = @db.collection(lang)
+
+    #c.find.each do |item|
+    #  if item.has_key?('string')
+    #    strings[item['name']] = item['string']
+    #  elsif item.has_key?('str_ar')
+    #    str_ars[item['name']] = item['str_ar']
+    #  elsif item.has_key?('str_pl')
+    #    str_pls[item['name']] = item['str_pl']
+    #  end
+    #end
+
+    strs = c.find_one()
+
+    { :strings => strs['strings'],
+      :str_ars => strs['str_ars'],
+      :str_pls => strs['str_pls'] }
   end
 
   def put_strings(lang, strings)
-    # For each item, update the timestamp if and only if the item has changed
+    c = @db.collection(lang)
+
+    c.insert(strings)
+
+    #strings[:strings].each do |k, v|
+    #  c.insert({:name => k, :string => v.values.first})
+    #end
+
+    #strings[:str_ars].each do |k, v|
+    #  c.insert({:name => k, :str_ar => v})
+    #end
+
+    #strings[:str_pls].each do |k, v|
+    #  c.insert({:name => k, :str_pl => v})
+    #end
   end
 end

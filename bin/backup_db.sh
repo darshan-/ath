adbe419be0aa80e4d50cb52a56843a3b1349b416
Darshan-Josiah Barber <darshan@darshancomputing.com>
2011-10-08 19:11:25 -0700

    Set up hourly backup to Google Storage.

---
+++ bin/backup_db.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+d=`date +%Y%m%d%H%M`
+gsd="gs://darshancomputing/mongo-dump-current"
+
+cd ~/mongo-dumps/
+rm -rf *
+
+mongodump -o $d
+tar czf $d.tgz $d
+rm -rf $d
+
+gsutil rm $gsd/*
+gsutil cp $d.tgz $gsd/$d.tgz
